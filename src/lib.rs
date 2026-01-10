c1b303862f7659f1415124d656d97012dfcb26f9
ElHuzain <a.huzain98@gmail.com>
2026-01-10 07:02:23 +0200

    refactor: using iterators in search

--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,10 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line.trim());
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
